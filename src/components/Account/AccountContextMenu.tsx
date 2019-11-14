e5e20fb2e59400c220fa722c0e0648433786aca8
Marcel-Ebert <marcel@wolfgang-ebert.de>
2019-11-14 18:26:08 +0100

    Add Popper WIP

--- src/components/Account/AccountContextMenu.tsx
+++ src/components/Account/AccountContextMenu.tsx
@@ -2,7 +2,6 @@ import React from "react"
 import Divider from "@material-ui/core/Divider"
 import ListItemIcon from "@material-ui/core/ListItemIcon"
 import ListItemText from "@material-ui/core/ListItemText"
-import Menu from "@material-ui/core/Menu"
 import MenuItem from "@material-ui/core/MenuItem"
 import CallMadeIcon from "@material-ui/icons/CallMade"
 import MoneyIcon from "@material-ui/icons/AttachMoney"
@@ -12,6 +11,12 @@ import { Account } from "../../context/accounts"
 import { SettingsContextType } from "../../context/settings"
 import { useIsMobile } from "../../hooks/userinterface"
 import ContextMenu, { AnchorRenderProps } from "../ContextMenu"
+import Slide from "@material-ui/core/Slide"
+import ClickAwayListener from "@material-ui/core/ClickAwayListener"
+import Popper from "@material-ui/core/Popper"
+import MenuList from "@material-ui/core/MenuList"
+import Paper from "@material-ui/core/Paper"
+import makeStyles from "@material-ui/core/styles/makeStyles"
 
 interface ItemProps {
   disabled?: boolean
@@ -50,46 +55,68 @@ interface MenuProps {
   settings: SettingsContextType
 }
 
+const useStyles = makeStyles(theme => ({
+  root: {},
+  paper: {
+    position: "fixed",
+    left: 0,
+    bottom: 0,
+    visibility: "visible"
+  }
+}))
+
 function AccountContextMenu(props: MenuProps) {
   const isSmallScreen = useIsMobile()
+  const classes = useStyles()
+
   return (
-    <ContextMenu
-      anchor={props.children}
-      menu={({ anchorEl, open, onClose, closeAndCall }) => (
-        <Menu
-          anchorEl={anchorEl || undefined}
-          disableAutoFocusItem={isSmallScreen}
-          open={open}
-          onClose={onClose}
-          MenuListProps={MenuListProps}
-        >
-          <AccountContextMenuItem
-            disabled={!props.activated}
-            icon={<SwapHorizIcon style={{ transform: "scale(1.2)" }} />}
-            label="Trade"
-            onClick={closeAndCall(props.onTrade)}
-          />
-          <AccountContextMenuItem
-            disabled={!props.activated}
-            icon={<CallMadeIcon />}
-            label="Withdraw"
-            onClick={closeAndCall(props.onWithdraw)}
-          />
-          <Divider />
-          <AccountContextMenuItem
-            disabled={!props.activated}
-            icon={<MoneyIcon />}
-            label="Assets & Balances"
-            onClick={closeAndCall(props.onManageAssets)}
-          />
-          <AccountContextMenuItem
-            icon={<SettingsIcon />}
-            label="Account Settings"
-            onClick={closeAndCall(props.onAccountSettings)}
-          />
-        </Menu>
-      )}
-    />
+    <div className={classes.root}>
+      <ContextMenu
+        anchor={props.children}
+        menu={({ anchorEl, open, onClose, closeAndCall }) => (
+          <Popper placement="bottom" open={open} anchorEl={anchorEl || undefined} transition disablePortal>
+            {({ TransitionProps, placement }) => (
+              <Slide
+                {...TransitionProps}
+                direction="up"
+                style={{ transformOrigin: placement === "bottom" ? "center top" : "center bottom" }}
+              >
+                <Paper className={classes.paper}>
+                  <ClickAwayListener onClickAway={onClose}>
+                    <MenuList autoFocusItem={open} id="menu-list-grow">
+                      <AccountContextMenuItem
+                        disabled={!props.activated}
+                        icon={<SwapHorizIcon style={{ transform: "scale(1.2)" }} />}
+                        label="Trade"
+                        onClick={closeAndCall(props.onTrade)}
+                      />
+                      <AccountContextMenuItem
+                        disabled={!props.activated}
+                        icon={<CallMadeIcon />}
+                        label="Withdraw"
+                        onClick={closeAndCall(props.onWithdraw)}
+                      />
+                      <Divider />
+                      <AccountContextMenuItem
+                        disabled={!props.activated}
+                        icon={<MoneyIcon />}
+                        label="Assets & Balances"
+                        onClick={closeAndCall(props.onManageAssets)}
+                      />
+                      <AccountContextMenuItem
+                        icon={<SettingsIcon />}
+                        label="Account Settings"
+                        onClick={closeAndCall(props.onAccountSettings)}
+                      />
+                    </MenuList>
+                  </ClickAwayListener>
+                </Paper>
+              </Slide>
+            )}
+          </Popper>
+        )}
+      />
+    </div>
   )
 }
 
